016e79ce12f79711b407d79ba82b379b28845264
varian <mizeyuyu@gmail.com>
2020-07-25 15:53:03 +0800

    update

--- source-DeepClone/deepClone.js
+++ source-DeepClone/deepClone.js
@@ -1,34 +1,40 @@
 class DeepClone {
+  // 1. 递归克隆 2. 对象分类讨论 3. 缓存处理环
   constructor() {
     this.cacheList = [];
   }
   clone(source) {
     if (source instanceof Object) {
-      const cache = this.findCache(source);
+      const cache = this.findCache(source); // 如果找到缓存，直接返回
       if (cache) return cache;
       else {
         let target;
-        if (source instanceof Array) {
+        if (target instanceof Array) {
           target = new Array();
-        } else if (source instanceof Function) {
+        } else if (target instanceof Function) {
           target = function () {
             return source.apply(this, arguments);
           };
-        } else if (source instanceof Date) {
+        } else if (target instanceof Date) {
           target = new Date(source);
-        } else if (source instanceof RegExp) {
+        } else if (target instanceof RegExp) {
           target = new RegExp(source.source, source.flags);
+        } else {
+          target = new Object();
         }
-        this.cacheList.push([source, target]);
+
+        this.cacheList.push([source, target]); // 把原对象和新对象放进缓存列表
         for (let key in source) {
           if (source.hasOwnProperty(key)) {
-            target[key] = this.clone(source[key]);
+            // 不拷贝原型上的属性，浪费内存
+            target[key] = this.clone(source[key]); // 递归
           }
         }
         return target;
       }
+    } else {
+      return source;
     }
-    return source;
   }
   findCache(source) {
     for (let i = 0; i < this.cacheList.length; ++i) {
@@ -36,6 +42,16 @@ class DeepClone {
         return this.cacheList[i][1];
       }
     }
-    return undefined;
   }
 }
+
+function test() {
+  console.log("测试环");
+  const obj = { name: "hh" };
+  obj.self = obj;
+  const obj2 = new DeepClone().clone(obj);
+  console.assert(obj !== obj2);
+  console.assert(obj.name === obj2.name);
+  console.assert(obj.self !== obj2.self);
+}
+test();
